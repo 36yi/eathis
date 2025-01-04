5a86560009c4e9c5a76fa486bb2d6de9aff4a77a
yunjaekwon <36yunjae@gmail.com>
2025-01-05 02:45:37 +0900

    before getting values from db

--- eathis_springboot/src/main/java/com/example/eathis/controller/GeminiController.java
+++ eathis_springboot/src/main/java/com/example/eathis/controller/GeminiController.java
@@ -4,8 +4,11 @@ import com.example.eathis.service.GeminiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 
 @RestController
+@RequestMapping("/api")
 public class GeminiController {
 
     private final GeminiService geminiService;
@@ -15,13 +18,17 @@ public class GeminiController {
         this.geminiService = geminiService;
     }
 
-    @GetMapping("/api/symbols")
+    @GetMapping("/symbols")
     public String getSymbols() {
         return geminiService.getSymbols();
     }
 
     @PostMapping("/generate")
-    public String generateContent(@RequestBody String prompt) {
-        return geminiService.generateContent(prompt);
+    public void generateContent(@RequestBody Map<String, int[]> request) { //배열이 올거라 가정하자
+        int[] members = (int[])request.get("members");
+        for (int member : members) {
+            System.out.println(member);
+        }
+//        return geminiService.generateContent(request.get());
     }
 }
